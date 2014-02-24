f7f918f6e95ee5b42a29b00ed365b17bb88004e4
Max Woolf <max@minimaxir.com>
2014-02-23 23:55:55 -0800

    initial

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 hacker-news-download-all-stories
 ================================
 
-Download *ALL* the submissions from Hacker News
+Download *ALL* the submissions from Hacker News using the official API.
+
+Requires Python 2.7 and pandas.
---
+++ hacker_news_getallstories.py
@@ -0,0 +1,36 @@
+import urllib2
+import json
+import datetime
+import time
+import pytz
+import pandas as pd
+from pandas import DataFrame
+
+ts = str(int(time.time()))
+iterations = 1268
+df = DataFrame()
+hitsPerPage = 1000
+requested_keys = ["title","url","points","num_comments","author","created_at_i","objectID"]
+
+
+for i in xrange(iterations):
+	try:
+		url = 'https://hn.algolia.com/api/v1/search_by_date?tags=story&hitsPerPage=%s&numericFilters=created_at_i<%s' % (hitsPerPage, ts)
+		req = urllib2.Request(url)
+		response = urllib2.urlopen(req)
+		data = json.loads(response.read())
+		data = DataFrame(data["hits"])[requested_keys]
+		df = df.append(data,ignore_index=True)
+		ts = data.created_at_i.min()
+		print i
+		time.sleep(3.6)
+		
+	except Exception, e:
+		print e
+		
+df["title"] = df["title"].map(lambda x: x.translate(dict.fromkeys([0x201c, 0x201d, 0x2011, 0x2013, 0x2014, 0x2018, 0x2019, 0x2026, 0x2032])).encode('utf-8').replace(',',''))
+df["created_at"] = df["created_at_i"].map(lambda x: datetime.datetime.fromtimestamp(int(x), tz=pytz.timezone('America/New_York')).strftime('%Y-%m-%d %H:%M:%S'))
+
+ordered_df = df[["title","url","points","num_comments","author","created_at","objectID"]]
+
+ordered_df.to_csv("hacker_news_stories.csv",encoding='utf-8', index=False)
