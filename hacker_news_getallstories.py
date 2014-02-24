062ed90c99d02e70d2285ff6865d167de603a51e
Jared Sohn <jared.sohn@gmail.com>
2014-02-24 01:26:25 -0800

    No longer hardcodes number of iterations
    Doing so means that you will likely miss some of the earlier stories (even if you edit the script right before running it.)  You can test this easily (as I did) by adding a tag for author_[some username with small number of stories] and setting hitsPerPage smaller than that number, and verifying that your output includes the oldest story.  It might also make sense to include a commandline option that does that switches into that mode for you.

--- hacker_news_getallstories.py
+++ hacker_news_getallstories.py
@@ -7,27 +7,31 @@ import pandas as pd
 from pandas import DataFrame
 
 ts = str(int(time.time()))
-iterations = 1268
 df = DataFrame()
 hitsPerPage = 1000
 requested_keys = ["title","url","points","num_comments","author","created_at_i","objectID"]
 
+i = 0
 
-for i in xrange(iterations):
+while True:
 	try:
 		url = 'https://hn.algolia.com/api/v1/search_by_date?tags=story&hitsPerPage=%s&numericFilters=created_at_i<%s' % (hitsPerPage, ts)
 		req = urllib2.Request(url)
 		response = urllib2.urlopen(req)
 		data = json.loads(response.read())
+		last = data["nbHits"] < hitsPerPage
 		data = DataFrame(data["hits"])[requested_keys]
 		df = df.append(data,ignore_index=True)
 		ts = data.created_at_i.min()
 		print i
+		if (last):
+			break
 		time.sleep(3.6)
-		
+		i += 1
+
 	except Exception, e:
 		print e
-		
+
 df["title"] = df["title"].map(lambda x: x.translate(dict.fromkeys([0x201c, 0x201d, 0x2011, 0x2013, 0x2014, 0x2018, 0x2019, 0x2026, 0x2032])).encode('utf-8').replace(',',''))
 df["created_at"] = df["created_at_i"].map(lambda x: datetime.datetime.fromtimestamp(int(x), tz=pytz.timezone('America/New_York')).strftime('%Y-%m-%d %H:%M:%S'))
 
